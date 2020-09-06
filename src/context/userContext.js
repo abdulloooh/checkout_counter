e479876c8ec82e0180765274c2c0a62c683d4f1c
abdulloooh <abdullahakinwumi@gmail.com>
2020-09-06 04:51:13 +0100

    referenced doc

--- src/context/userContext.js
+++ src/context/userContext.js
@@ -4,3 +4,9 @@ const UserContext = React.createContext();
 UserContext.displayName = "UserContext";
 
 export default UserContext;
+
+/**
+ * More read:
+ * Particularly on multiple contexts: https://stackoverflow.com/questions/53988193/how-to-get-multiple-static-contexts-in-new-context-api-in-react-v16-6
+ * React Doc: https://reactjs.org/docs/context.html
+ */
